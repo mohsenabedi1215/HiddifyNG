9383acbb229c730ff1ae355f9ecdf132725825a0
Hiddify1 <Hiddify@gmail.com>
2023-05-14 17:07:12 +0200

    new: send logcat as a file

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/LogcatActivity.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/LogcatActivity.kt
@@ -1,24 +1,26 @@
 package com.v2ray.ang.ui
 
-import android.os.Handler
-import android.os.Looper
-import android.os.Bundle
+import android.content.Intent
+import android.net.Uri
+import android.os.*
 import android.text.method.ScrollingMovementMethod
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.core.content.FileProvider
 import androidx.lifecycle.lifecycleScope
 import com.v2ray.ang.AppConfig.ANG_PACKAGE
+import com.v2ray.ang.BuildConfig
 import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivityLogcatBinding
 import com.v2ray.ang.extension.toast
 import com.v2ray.ang.util.Utils
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-
+import java.io.File
+import java.io.FileWriter
 import java.io.IOException
-import java.util.LinkedHashSet
+
 
 class LogcatActivity : BaseActivity() {
     private lateinit var binding: ActivityLogcatBinding
@@ -59,14 +61,15 @@ class LogcatActivity : BaseActivity() {
 //                val bufferedReader = BufferedReader(
 //                        InputStreamReader(process.inputStream))
 //                val allText = bufferedReader.use(BufferedReader::readText)
-                val allText = process.inputStream.bufferedReader().use { it.readText() }
+                var allText = process.inputStream.bufferedReader().use { it.readText() }
                 var regex=Regex("""\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2}\.\d{3}\s+([A-Z]\/[A-z.-]*)\s*\(?\d*\)?:?\|?\s*""")
                 val regex_xray = Regex("""^\d{4}/\d{2}/\d{2}\s+\d{2}:\d{2}:\d{2}\s*""")
 
                 var regex2=Regex("""  +""")
-
+                allText=allText.replace(regex,"").replace(regex_xray,"").replace(regex2,"")
+                send2telegram(allText)
                 launch(Dispatchers.Main) {
-                    binding.tvLogcat.text = allText.replace(regex,"").replace(regex_xray,"").replace(regex2,"")
+                    binding.tvLogcat.text =allText
                     binding.tvLogcat.textDirection=View.TEXT_DIRECTION_LTR
                     binding.tvLogcat.movementMethod = ScrollingMovementMethod()
                     binding.tvLogcat.scrollTo(0,binding.tvLogcat.length())
@@ -77,6 +80,48 @@ class LogcatActivity : BaseActivity() {
         } catch (e: IOException) {
             e.printStackTrace()
         }
+    }
+
+    private fun send2telegram(allText: String) {
+        val filename = "logcat${BuildConfig.VERSION_CODE}-${Build.VERSION.SDK_INT}.txt"
+        //FileOutputStream fos_FILE_eulerY = null;
+        //FileOutputStream fos_FILE_eulerY = null;
+        val externalFilesDirectory: File? = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+        val textFile = File(externalFilesDirectory, filename)
+        val message = allText
+        try {
+            //fos_FILE_eulerY = openFileOutput(textFile.getAbsolutePath() , MODE_PRIVATE);
+            //fos_FILE_eulerY.write(message.getBytes());
+            val writer = FileWriter(textFile)
+            writer.append(message)
+            writer.flush()
+            writer.close()
+            val fileUri: Uri = FileProvider.getUriForFile(this, "$packageName.provider", textFile)
+            val emailIntent = Intent(Intent.ACTION_SEND)
+            emailIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            // set the type to 'email'
+            // set the type to 'email'
+            emailIntent.type = "text/plain"
+            // the attachment
+            // the attachment
+            emailIntent.putExtra(Intent.EXTRA_STREAM, fileUri)
+            // the mail subject
+            // the mail subject
+            startActivity(Intent.createChooser(emailIntent, "Send logcat ..."))
+        } catch (e: Exception) {
+            e.localizedMessage
+            e.printStackTrace()
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, allText)
+            intent.type = "text/plain"
+            val chooserTitle = "Send Logcat to..."
+            val chooser = Intent.createChooser(intent, chooserTitle)
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(chooser)
+            }
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
